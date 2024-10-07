5f6b202be75692ed022619d48a40efabb121be2d
siddastic <siddhantgaurav2003@gmail.com>
2024-10-08 00:13:14 +0530

    fetch current user location

--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -1,7 +1,10 @@
 import 'dart:async';
 
+import 'package:flutter/cupertino.dart' show CupertinoActivityIndicator;
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:ridesense/services/location.dart';
 import 'package:ridesense/widgets/input.dart';
 
 class MapScreen extends StatefulWidget {
@@ -22,15 +25,61 @@ class _MapScreenState extends State<MapScreen> {
     zoom: 14.4746,
   );
 
+  LatLng? _currentUserLocation; // Variable to store the current user location
+  final Set<Marker> _markers = {}; // To store map markers
+  bool isLocationLoading = false;
+
   @override
   void initState() {
+    super.initState();
     WidgetsBinding.instance.addPostFrameCallback((timeStamp) async {
-      // animate camera to the search result
       LatLng location = ModalRoute.of(context)!.settings.arguments as LatLng;
       final GoogleMapController controller = await _controller.future;
       controller.animateCamera(CameraUpdate.newLatLng(location));
+
+      // Add search result marker to the map
+      _markers.add(Marker(
+        markerId: const MarkerId('search_result_marker'),
+        position: location,
+      ));
+      setState(() {});
     });
-    super.initState();
+  }
+
+  // Method to fetch and show current user location using LocationService
+  Future<void> _getCurrentLocation() async {
+    setState(() {
+      isLocationLoading = true;
+    });
+    try {
+      // Get current position using LocationService
+      Position position = await LocationService.determinePosition();
+      LatLng currentLatLng = LatLng(position.latitude, position.longitude);
+
+      setState(() {
+        _currentUserLocation = currentLatLng;
+
+        // Move the camera to the current location
+        _controller.future.then((controller) {
+          controller
+              .animateCamera(CameraUpdate.newLatLng(_currentUserLocation!));
+        });
+
+        // Add a blue dot marker for the current location
+        _markers.add(Marker(
+          markerId: const MarkerId('current_location_marker'),
+          position: _currentUserLocation!,
+          icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueBlue),
+        ));
+      });
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text(e.toString())),
+      );
+    }
+    setState(() {
+      isLocationLoading = false;
+    });
   }
 
   @override
@@ -59,16 +108,23 @@ class _MapScreenState extends State<MapScreen> {
               onMapCreated: (GoogleMapController controller) {
                 _controller.complete(controller);
               },
-              markers: {
-                Marker(
-                  markerId: MarkerId('search_result_marker'),
-                  position: searchResult,
-                ),
-              },
+              markers: _markers,
             ),
           ),
         ],
       ),
+      floatingActionButtonLocation: isLocationLoading
+          ? FloatingActionButtonLocation.centerFloat
+          : FloatingActionButtonLocation.endFloat,
+      floatingActionButton: FloatingActionButton(
+          onPressed: isLocationLoading ? null : _getCurrentLocation,
+          backgroundColor: Colors.blue,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(100),
+          ),
+          child: isLocationLoading
+              ? const CupertinoActivityIndicator()
+              : const Icon(Icons.my_location)),
     );
   }
 }
--- lib/services/location.dart
+++ lib/services/location.dart
@@ -1,3 +1,4 @@
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:geocoding/geocoding.dart';
 
@@ -5,5 +6,39 @@ class LocationService {
   static Future<LatLng> getCoordinates(String location) async {
     List<Location> locations = await locationFromAddress(location);
     return LatLng(locations[0].latitude, locations[0].longitude);
+  }
+
+  static Future<Position> determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // Test if location services are enabled.
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // Location services are not enabled don't continue
+      // accessing the position and request users of the
+      // App to enable the location services.
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // Permissions are denied, next time you could try
+        // requesting permissions again (this is also where
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      // Permissions are denied forever, handle appropriately.
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    // When we reach here, permissions are granted and we can
+    // continue accessing the position of the device.
+    return await Geolocator.getCurrentPosition();
   }
 }
